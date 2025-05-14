434bc6839d566a8caae7b2959258dd8e33ec7aa8
6a6ak <babak.ahari@gmail.com>
2025-05-15 00:06:18 +0200

    Improve code comments and formatting in main.c for clarity

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
-#define F_CPU 16000000UL
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
+#define F_CPU 16000000UL  // 16MHz clock speed
+#include <avr/io.h> //Rigister sssumption like DDRx, PORTx, PINx
+#include <util/delay.h> // For delay functions
+#include <stdlib.h> // For itoa function convert integer to string
 
 // UART config
 #define BAUD 9600
 #define MYUBRR (F_CPU/16/BAUD - 1)
 
-// HC-SR04 pins on Arduino Mega
-#define TRIG_PIN PE4  // pin 2
-#define ECHO_PIN PE5  // pin 3
+// HC-SR04 pins on Arduino Mega in Microcontroler ATMega 2560 placed on PORT E & not on PORT D
+#define TRIG_PIN PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
+#define ECHO_PIN PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
 
-void uart_init(unsigned int ubrr) {
-    UBRR0H = (unsigned char)(ubrr >> 8);
-    UBRR0L = (unsigned char)ubrr;
-    UCSR0B = (1 << TXEN0);
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+void uart_init(unsigned int ubrr) { // Initialize UART
+    UBRR0H = (unsigned char)(ubrr >> 8); // Set baud rate
+    UBRR0L = (unsigned char)ubrr; // Set baud rate
+    UCSR0B = (1 << TXEN0); // Enable transmitter
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // Set frame format: 8 data bits, no parity, 1 stop bit
 }
 
 void uart_transmit(unsigned char data) {
