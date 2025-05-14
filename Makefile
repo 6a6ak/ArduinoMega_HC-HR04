c3958281cd3cddfae2d668679a06e62075c607b7
6a6ak <babak.ahari@gmail.com>
2025-05-14 22:16:28 +0200

    Update Makefile, main.c, diagram.json, and wokwi.toml for Arduino Mega 2560 compatibility

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
-MCU = atmega328p
+MCU = atmega2560
 F_CPU = 16000000UL
 TARGET = main
-CC="C:\avr\bin\avr-gcc"
-OBJCOPY="C:\avr\bin\avr-objcopy"
-CFLAGS = -mmcu=$(MCU) -DF_CPU=$(F_CPU) -Wall -Os -I./drive
+
+CC = "C:\avr\bin\avr-gcc"
+OBJCOPY = "C:\avr\bin\avr-objcopy"
+CFLAGS = -mmcu=$(MCU) -DF_CPU=$(F_CPU) -Wall -Os
 
 SRC = main.c
 OBJ = $(SRC:.c=.o)
--- main.c
+++ main.c
@@ -1,75 +1,67 @@
-#define F_CPU 16000000UL
-
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdlib.h>  // for itoa()
+#include <stdlib.h>
 
-#define TRIG_PIN PD2
-#define ECHO_PIN PD3
+#define F_CPU 16000000UL
+#define BAUD 9600
+#define MYUBRR (F_CPU/16/BAUD - 1)
+
+// --- Mega pins ---
+#define TRIG_PIN PE4  // Arduino pin 2
+#define ECHO_PIN PE5  // Arduino pin 3
 
 void uart_init(unsigned int ubrr) {
-    // Set baud rate
     UBRR0H = (unsigned char)(ubrr >> 8);
     UBRR0L = (unsigned char)ubrr;
-    // Enable transmitter
     UCSR0B = (1 << TXEN0);
-    // Set frame format: 8 data bits, 1 stop bit
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-void uart_send_char(char c) {
+void uart_transmit(unsigned char data) {
     while (!(UCSR0A & (1 << UDRE0)));
-    UDR0 = c;
+    UDR0 = data;
 }
 
-void uart_send_string(const char* str) {
+void uart_print(const char* str) {
     while (*str) {
-        uart_send_char(*str++);
+        uart_transmit(*str++);
     }
 }
 
-void ultrasonic_init() {
-    DDRD |= (1 << TRIG_PIN);   // TRIG output
-    DDRD &= ~(1 << ECHO_PIN);  // ECHO input
-}
-
-void ultrasonic_trigger() {
-    PORTD &= ~(1 << TRIG_PIN);
+uint16_t read_distance_cm() {
+    PORTE &= ~(1 << TRIG_PIN);
     _delay_us(2);
-    PORTD |= (1 << TRIG_PIN);
+    PORTE |= (1 << TRIG_PIN);
     _delay_us(10);
-    PORTD &= ~(1 << TRIG_PIN);
-}
+    PORTE &= ~(1 << TRIG_PIN);
 
-uint16_t ultrasonic_read_cm() {
+    while (!(PINE & (1 << ECHO_PIN)));
+
     uint32_t count = 0;
-    uint16_t distance;
-
-    ultrasonic_trigger();
-
-    while (!(PIND & (1 << ECHO_PIN)));  // wait for ECHO high
-
-    while (PIND & (1 << ECHO_PIN)) {
+    while (PINE & (1 << ECHO_PIN)) {
         _delay_us(1);
         count++;
         if (count > 30000) break;
     }
 
-    distance = (uint16_t)(count / 58.0);
-    return distance;
+    return (uint16_t)(count * 0.034 / 2);
 }
 
 int main(void) {
-    char buffer[10];
-    uart_init(103);  // 9600 baud for 16MHz (UBRR = 103)
-    ultrasonic_init();
+    DDRE |= (1 << TRIG_PIN);
+    DDRE &= ~(1 << ECHO_PIN);
+
+    uart_init(MYUBRR);
+
+    char buffer[16];
+    uint16_t distance;
 
     while (1) {
-        uint16_t distance = ultrasonic_read_cm();
+        distance = read_distance_cm();
         itoa(distance, buffer, 10);
-        uart_send_string("Distance: ");
-        uart_send_string(buffer);
-        uart_send_string(" cm\r\n");
+        uart_print("Distance: ");
+        uart_print(buffer);
+        uart_print(" cm\r\n");
         _delay_ms(500);
     }
 }
